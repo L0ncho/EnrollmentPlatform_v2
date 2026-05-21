8657b6a0e6e499413cec4ca0c35480466bbca2a9
Gustavo Dominguez <gus.dominguez@duocuc.cl>
2026-05-21 17:52:02 -0400

    Initial commit

---
+++ src/main/java/com/duoc/EnrollmentPlatform/EnrollmentPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.EnrollmentPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollmentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollmentPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/EnrollmentPlatform/EnrollmentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.EnrollmentPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
